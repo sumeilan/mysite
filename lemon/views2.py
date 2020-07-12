0bcd98b802b2a78a3358de975fddc3c803711c37
sumeilan <912041511@qq.com>
2020-07-12 23:01:23 +0800

    20200621

---
+++ lemon/views2.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+import method, requests
+import json, time, os
+
+
+class Page(View):
+    page = "index.html"
+    def get(self,request):
+        return render(request, self.page)
+
+
+def testapi(request):
+    pt = method.cls_api()
+    exr = request.POST.get('exr', None)
+    title = "这么久没见2"
+    data = ""
+    data1 = ""
+    if request.method == 'POST':
+        data = pt.post(request.POST.get('url', None), json.loads(request.POST.get('testdate', None)))
+        result = data.json()
+        data1 = result['message']
+        if int(result['message'] == int(exr)):
+            data = 'pass'
+        else:
+            data = 'fail'
+    return render(request, "testapi.html", {"data": data, "data1": data1, "title": title})
+
+
+def add_args(a, b):
+    x = int(a)
+    y = int(b)
+    return x + y
+
+
+def send_url(env, path, header, body):
+    # url = 'http://lemondream.chumanapp.com/api/banner/get_banner_list'
+    if env == 'demo':
+        url = 'http://lemondream.chumanapp.com' + path
+    elif env == 'api2':
+        url = 'http://api-api2.lemondream.cn' + path
+    else:
+        url = 'http://api.lemondream.cn' + path
+
+    body = body
+    headers = eval(header)
+    response = requests.post(url, body, headers=headers, verify=False)
+    return HttpResponse(response, content_type="application/json")
+
+
+def testp(request):
+    if request.method == 'POST':
+        if request.POST:
+            env = request.POST.get('env', None)
+            path = request.POST.get('path', None)
+            header = request.POST.get('header', None)
+            body = request.POST.get('body', None)
+            res = send_url(env, path, header, body)
+            return HttpResponse(res)
+        else:
+            return HttpResponse(u'输入为空')
+    else:
+        a = 1
+        b = 2
+        res = add_args(a, b)
+        return HttpResponse(res)
+
+
+def post(request):
+    if request.method == 'POST':
+        d = {}
+        if request.POST:
+            a = request.POST.get('a', None)
+            b = request.POST.get('b', None)
+            if a and b:
+                res = add_args(a, b)
+                d['message1'] = res
+                d = json.dumps(d)
+                return HttpResponse(d)
+            else:
+                return HttpResponse(u'输入错误')
+        else:
+            return HttpResponse(u'输入为空')
+    else:
+        return HttpResponse(u'方法错误')
+
+
+def connect_adb(request):
+    # swipe = "adb shell input swipe 500 1000 500 10"
+    swipe = "adb devices"
+    re = os.popen(swipe)
+    return HttpResponse(re)
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from lemon import views
+from lemon import views,excel_views
+from lemon import views2
 
 urlpatterns = [
     # path('index/',views.index, name='index'),
     path('index/', views.Page.as_view(page="index.html"), name='index'),
     path('test/', views.Page.as_view(page="test1.html"), name='test'),
     path('lemon/', views.Page.as_view(page="lemon.html"), name='lemon'),
+    path('excel/', views.Page.as_view(page="excel.html"), name='excel'),
     path('testcase/', views.Page.as_view(page="testcase.html"), name='testcase'),
-    url('add/', views.post),
+    url('add/', views2.post),
     path('testp/', views.testp),
-    path('testapi/', views.testapi),
+    path('testapi/', views2.testapi),
     path('connect_adb/', views.connect_adb),
+    path('handel_excel/', excel_views.handel_excel),
 ]
