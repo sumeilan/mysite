11c10ec6fc51d295552c433d2ad12d7912db1702
sumeilan <912041511@qq.com>
2020-08-05 18:12:07 +0800

    20200805

--- lemon/tool_views.py
+++ lemon/tool_views.py
@@ -26,18 +26,24 @@ def handel_csv(request):
 
 def save_case(request):
     if request.POST:
+        file = request.POST.get("case_file", None)
+        module = request.POST.get("module", None)
         case_name = request.POST.get("case_name", None)
-        file1 = request.POST.get("module", None)
+        url = request.POST.get("c_path", None)
+        request_headers = request.POST.get("c_header", None)
+        request_parameter = request.POST.get("c_body", None)
+        expect_result = request.POST.get("e_result", None)
+        request_method = request.POST.get("method", None)
 
-    #python E:\python\tools\lemon\save_case.py E:\python\tools\datas\test2.xlsx 0 ["case_name", "module"]
 
-    file = 'E:\\python\\tools\\datas\\test2.xlsx'
-    sheet_id = "0"
-    case = ["case_name", "module", "url", "request_method", "request_headers", "request_parameter","data_from_response", "request_depend_data", "expect_result"]
-    case=",".join(case)
-    print(case)
+    sheet_id = module
+    # case = ["case_name", "module", "url", "request_method", "request_headers", "request_parameter", "expect_result"]
+    case = [case_name,module,url,request_method,request_headers,request_parameter,expect_result]
+    case = "@".join(case)
+    print("case",case)
 
     swipe = "python E:\\python\\tools\\lemon\\save_case.py " + file + " " + sheet_id + " " + case
+    print(swipe)
     re = os.popen(swipe)
     return HttpResponse(swipe)
 
